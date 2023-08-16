8e9f813dbb83056f2f56cf0d71a418099fc100a7
Louis Moses <louismoses.rait@gmail.com>
2023-08-16 15:04:17 +0800

    passing data with locals.variable

--- 4.2 Passing Data/index.js
+++ 4.2 Passing Data/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  const totalLetters = req.body["fName"].length + req.body["lName"].length;
+  res.render("index.ejs", { numLetters: totalLetters });
+});
 
 app.listen(port, () => {
   console.log(`server running at port ${port}`);
